4d811d9edfd614356f5d3cc29c7abee76c18ecc7
GreedIsGoody <norman.tsar@gmail.com>
2026-02-15 11:39:35 +0200

    first commit

---
+++ src/shared/api/tasks/index.js
@@ -0,0 +1,9 @@
+import localAPI from "./local";
+import serverAPI from './server'
+
+const isLocal = import.meta.env.VITE_STATIC_BACKEND === 'true'
+
+const taskAPI =  isLocal ? localAPI : serverAPI
+
+
+export default taskAPI
